5a30043f4c7ed99cc057411dedf825f090fbe93f
LucianoVilla95 <Luciano121195@gmail.com>
2022-06-17 19:38:16 -0300

    first commit

--- 04-EstructuraDeDatos-I/homework/homework.js
+++ 04-EstructuraDeDatos-I/homework/homework.js
@@ -15,10 +15,16 @@ Como ejercicio adicional y completamente opcional, al terminar de resolver este 
 */
 
 function nFactorial(n) {
+  if(n > -1 && n < 2) return 1;
+  else if(n < 0) return 0;
+  return n * nFactorial(n - 1);
 }
 
 function nFibonacci(n) {
-}
+  if(n === 0) return 0;
+  if(n === 1) return 1;
+  return nFibonacci(n - 1) + nFibonacci(n - 2);
+}  
 
 /*
 Implementar la clase Queue, sabiendo que es una estructura de tipo FIFO, donde el primer elemento que ingresa es el primero que se quita. Definir los siguientes métodos:
@@ -30,8 +36,21 @@ Pueden utilizar class o función constructora.
 */
 
 function Queue() {
-
+  this.queue = [];
 }
+
+Queue.prototype.enqueue = function(value) {
+  this.queue.push(value);
+}
+
+Queue.prototype.dequeue = function() {
+  return this.queue.shift();
+}
+
+Queue.prototype.size = function() {
+  return this.queue.length;
+}
+
 
 // No modifiquen nada debajo de esta linea
 // --------------------------------
